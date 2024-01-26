48c5ed8e9723734858a424b67c729f7243de2d1b
Joscelyn Jean <joscelyn.jean@montreal.ca>
2024-01-26 13:19:16 -0500

    Text extraction

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Experimenting a combinaison of Tesseract OCR, OpenCV and machine learning to ext
 2. Generate multiple images from source (original, without border, etc.)
 3. Extract texts from images and original file
 4. Use it as input for ML to get the right value
+
+# Development
+
+## Prerequisites
+
+### MacOSX
+
+1. Install tesseract using homebrew : `brew install tesseract`;
---
+++ src/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+import pdf2image
+import pytesseract
+
+app = Flask(__name__)
+
+def pdf_to_image(pdf_content):
+    return pdf2image.convert_from_bytes(pdf_content, fmt="jpeg")
+
+def image_to_text(image_content):
+    return pytesseract.image_to_string(image_content)
+
+@app.route("/invoice-processing-requests", methods=["POST"])
+def process_invoice():
+    
+    # Read content of the file
+    uploaded_file = request.files.get('invoice')
+    file_content = uploaded_file.stream.read()
+    
+    # Convert PDF file to image
+    image_content = pdf_to_image(file_content)
+    
+    # Extract text from image
+    ocr_content = image_to_text(image_content[0])
+    
+    print(file_content)
+    
+    return {
+        'documentType': '',
+        'gstAmount': '',
+        'qstAmount': '',
+        'taxAmount': '',
+        'totalAmount': '',
+        'purchaseOrder': '',
+        'gstNumber': '',
+        'qstNumber': '',
+        'ocrResult': ocr_content
+    }
+    
+if __name__ == "__main__":
+    app.run(debug=True)
